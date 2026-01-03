24fb8352d73fca5c7e78bbb7d498bc1d08fd4391
devjani07 <escapeball69@gmail.com>
2026-01-03 09:13:25 +0530

    Update backend logic and fixes

--- config/email.js
+++ config/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const createTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     secure: false,
